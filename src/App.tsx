9856017d053cc61d1f4325c266f45be049b39aa1
honorific <honorific.info@gmail.com>
2025-01-13 23:34:29 +0330

    using useLocalStorageHandler hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,52 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import { Todo } from './types/types'
 import useLocalStorageHandler from './hooks/useLocalStorageHandler'
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState<Todo[]>([
-    { id: 1, text: 'Learn React' },
-    { id: 2, text: 'Build a Todo App' },
-  ])
-  const [add, remove] = useLocalStorageHandler('')
+  const [todos, setTodos] = useState<Todo[] | []>([])
+  const { add, read } = useLocalStorageHandler()
   const [inputValue, setInputValue] = useState('')
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value)
   }
 
-  const handleAddTodo = () => {
+  useEffect(() => {
+    const sync = async () => {
+      const syncTodos = await read()
+      if (syncTodos.data) {
+        setTodos(syncTodos.data)
+      }
+    }
+    sync()
+  }, [])
+
+  useEffect(() => {
+    const adder = async (clear: Boolean) => {
+      await add(todos, clear)
+    }
+    todos.length ? adder(true) : adder(false)
+  }, [todos])
+
+  const handleAddTodo = async () => {
     if (inputValue.trim() !== '') {
       const newTodo: Todo = {
         id: Date.now(),
         text: inputValue.trim(),
       }
-      setTodos([...todos, newTodo])
-      add(todos)
       setInputValue('')
+      setTodos([...todos, newTodo])
     }
   }
 
-  const handleDeleteTodo = (id: number) => {
+  const handleDeleteTodo = async (id: number) => {
     setTodos(todos.filter((todo) => todo.id !== id))
-    remove(todos.find((todo) => todo.id === id))
   }
 
   return (
     <div className="App">
-      <h1>UU Todo App</h1>
+      <h1>Todo App</h1>
 
       <div>
         <input
@@ -51,17 +63,18 @@ const App: React.FC = () => {
         </button>
       </div>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <span>{todo.text}</span>
-            <button
-              className="delete-btn"
-              onClick={() => handleDeleteTodo(todo.id)}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
+        {todos.length > 0 &&
+          todos.map((todo) => (
+            <li key={todo.id}>
+              <span>{todo.text}</span>
+              <button
+                className="delete-btn"
+                onClick={() => handleDeleteTodo(todo.id)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
       </ul>
     </div>
   )

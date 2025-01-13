d174bc01edb33e38132fe1d8ab83ce65a523af71
honorific <honorific.info@gmail.com>
2025-01-13 16:12:03 +0330

    rewrite methods of useLocalStorage with promise

--- src/hooks/useLocalStorageHandler.ts
+++ src/hooks/useLocalStorageHandler.ts
@@ -2,32 +2,38 @@ import { Todo } from '../types/types'
 
 const useLocalStorageHandler = (val: Todo | Todo[] | String) => {
   const add = (val: Todo | Todo[]) => {
-    setTimeout(() => {
-      if (Array.isArray(val)) {
-        localStorage.clear()
-        val.map((v) => {
-          localStorage.setItem(String(v.id), v.text)
-        })
-      } else {
-        localStorage.setItem(String(val.id), val.text)
-      }
-    }, 3000)
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        if (Array.isArray(val)) {
+          localStorage.clear()
+          val.map((v) => {
+            localStorage.setItem(String(v.id), v.text)
+          })
+        } else {
+          localStorage.setItem(String(val.id), val.text)
+        }
+      }, 3000)
+      resolve({ status: 'ok' })
+    })
   }
   const remove = (val: Todo) => {
-    setTimeout(() => {
-      localStorage.removeItem(String(val.id))
-    }, 3000)
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        localStorage.removeItem(String(val.id))
+      }, 3000)
+      resolve({ status: 'ok' })
+    })
   }
 
   const read = () => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         const localStorageLength = localStorage.length
         let todos = []
         for (let i = 0; i < localStorageLength; i++) {
           todos.push(localStorage.key(i))
         }
-        resolve(todos)
+        resolve({ status: 'ok', data: todos })
       }, 3000)
     })
   }
